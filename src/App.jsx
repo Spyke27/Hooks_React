19e0988afc47cdcf8a9c8af7bc3c5064693338fa
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-01-25 22:15:39 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState("Rodrigo")
-  const [number, setNumber] = useState(0)
+  const [user, setUser] = useState()
+  
+  useEffect(() =>{
+    fetch("https://api.github.com/users/Spyke27")
+    .then((res) => res.json())
+    .then((json)=> setUser(json));
+  }, [])
 
-  const contLike = () => {
-    setNumber(number + 1)
-  }
+
 
   return (
     <div className="App">
-      <h1>Meu nome é: {name}</h1>
-      <label htmlFor="name">Nome: </label>
-      <input type="text" name="name" id="name" onChange={e => setName(e.target.value)}/>
-      <div>
-        <p>Likes: {number}</p>
-        <button onClick={contLike}>Like</button>
+
+      {user && (
+        
+        <div className='perfil'>
+        
+        <div className='avatar'><img src={user.avatar_url} alt="Foto Perfil" /></div>
+        <h1>{user.name}</h1>
+        <h2>{user.bio}</h2>
+
+        <div className='seguidores'>
+         <div className='followers'>
+         <p>seguidores</p>
+          <button>{user.followers}</button>
+         </div>
+         <div className='following'>
+         <p>seguindo</p>
+          <button>{user.following}</button>
+         </div>
+        </div>
+
       </div>
+        
+      )}
     </div>
   )
 }
-
 export default App
